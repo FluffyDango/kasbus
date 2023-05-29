3c7a704b16488437c7946f99094d440f3c54b313
Renaldas <renaldas1251@gmail.com>
2023-05-29 19:21:07 +0300

    Kipras and Renaldas: some design added and tried to use API

--- app/src/main/java/com/kasbus/kasbusapp/MainActivity.java
+++ app/src/main/java/com/kasbus/kasbusapp/MainActivity.java
@@ -2,13 +2,65 @@ package com.kasbus.kasbusapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String API_URL = "http://api.kasbus.lt/ratings/lgVf470llCAsb22TJA0f";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new FetchData().execute();
+    }
+
+    private class FetchData extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+            String data = "";
+            try {
+                URL url = new URL(API_URL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line = "";
+                while (line != null) {
+                    line = bufferedReader.readLine();
+                    data = data + line;
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return null;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String data) {
+            if (data == null) {
+                Log.e("Error", "Error fetching data");
+            } else {
+                // Here 'data' string contains the whole JSON data fetched from server
+                // Handle this JSON as per your need
+                Log.i("Data", data);
+            }
+        }
     }
 }
+
